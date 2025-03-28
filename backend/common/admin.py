7fe6ebdedfbfca768c752e84f49390e0d3958a20
reginaguermandi <reginaguermandi@gmail.com>
2025-03-28 15:17:10 -0300

    add book and book request routes

--- backend/common/admin.py
+++ backend/common/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User, Book, BookRequest, Address
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'is_active', 'is_staff', 'birth_date', 'phone')
+    search_fields = ('name', 'email')
+    list_filter = ('is_active', 'is_staff')
+    ordering = ('name',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Book)
+admin.site.register(BookRequest)
+admin.site.register(Address)
